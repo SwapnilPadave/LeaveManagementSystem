c47303b1f19143c3c0a148837825e80c40d1c83c
SwapnilPadave <spadave7@gmail.com>
2022-09-28 12:03:29 +0530

    Add project files.

---
+++ Contracts/ILeaveHistoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using LeaveManagementSystem.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaveManagementSystem.Contracts
+{
+    interface ILeaveHistoryRepository : IRepositoryBase<LeaveHistory>
+    {
+    }
+}
