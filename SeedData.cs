8bcc3fa1a027d07554a636f4a8d17931d8ac89a1
SwapnilPadave <spadave7@gmail.com>
2022-09-30 17:57:55 +0530

    adding roles based authenticatio in application

---
+++ SeedData.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaveManagementSystem
+{
+    public static class SeedData
+    {
+        public static void Seed(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            SeedRoles(roleManager);
+            SeedUsers(userManager);
+        }
+
+        private static void SeedUsers(UserManager<IdentityUser> userManager)
+        {
+            if (userManager.FindByNameAsync("Admin").Result == null)
+            {
+                var user = new IdentityUser
+                {
+                    UserName = "Admin",
+                    Email="Admin@localhost.com"
+                };
+                var resut = userManager.CreateAsync(user, "Admin@123").Result;
+                if (resut.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Administrator").Wait();
+                }
+            }
+        }
+
+        private static void SeedRoles(RoleManager<IdentityRole> roleManager)
+        {
+            if (!roleManager.RoleExistsAsync("Administrator").Result)
+            {
+                var role = new IdentityRole
+                {
+                    Name = "Administrator"
+                };
+                var result= roleManager.CreateAsync(role).Result;
+            }
+
+            if (!roleManager.RoleExistsAsync("Employee").Result)
+            {
+                var role = new IdentityRole
+                {
+                    Name = "Employee"
+                };
+                var result= roleManager.CreateAsync(role).Result;
+            }
+        }
+    }
+}
